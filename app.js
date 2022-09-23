fc0e0996745a390648781e416a082416e83fd47f
Martin Garibaldi <martin.garibaldi@gmail.com>
2022-09-23 16:13:24 -0300

    html features

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let viewsPath = path.resolve(__dirname, 'views')
 
 let app = express()
 
+app.use(express.static(__dirname +'/public'));
+
+
 app.listen(3000, function () {
   console.log("Ejercicio individual 1 corriendo...")
 })
